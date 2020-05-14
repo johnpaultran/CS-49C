8403e29acb34748295f502a2801c3447dc052ff4
John Paul Tran <56181958+johnpaultran@users.noreply.github.com>
2020-05-14 11:37:23 -0700

    Delete gameRule.h

--- 49C_project/gameRule.h
+++
@@ -1,7 +0,0 @@
-// header file for game rule functions
-
-// function prototypes
-int winningMove(int row, int player, int col, int connect_4[7][6]);
-int playerMove(int player, int turnCount, int connect_4[7][6]);
-int winningComputer(int player, int connect_4[7][6]);
-int AImove(int connect_4[7][6]);
