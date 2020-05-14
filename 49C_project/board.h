fab1ecd29905f6fc069318e2c17328d7a823c0bd
John Paul Tran <56181958+johnpaultran@users.noreply.github.com>
2020-05-14 11:37:54 -0700

    Add files via upload

---
+++ 49C_project/board.h
@@ -0,0 +1,5 @@
+// header files for game board functions
+
+// function prototypes
+int printBoard(int gameBoard[7][6]);
+int showMove(int column, int player, int gameBoard[7][6]);
---
+++ 49C_project/gameRule.h
@@ -0,0 +1,7 @@
+// header file for game rule functions
+
+// function prototypes
+int winningMove(int row, int player, int col, int gameBoard[7][6]);
+int playerMove(int player, int turnCount, int gameBoard[7][6]);
+int winningComputer(int player, int gameBoard[7][6]);
+int AImove(int gameBoard[7][6]);
